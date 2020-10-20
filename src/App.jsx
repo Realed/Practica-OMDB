31b100f811ef2384895671bd3362b29f519338fc
Daniel Gonzalez <danielgonzalezt13@gmail.com>
2020-10-20 11:31:49 +0200

    Added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
 //PAGES
-import HomePage from "./pages/Home"
+import LandingPage from "./pages/LandingPage"
 import RegisterPage from "./pages/Register"
 import SignInPage from "./pages/LogIn"
+import HomePage from "./pages/Home"
+import MoviesPage from "./pages/Movies"
+import ContactPage from "./pages/ContactUs"
 
 const App = () => {
   return (
     <>
       <Router>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/movies" component={MoviesPage} />
+        <Route exact path="/contactus" component={ContactPage} />
         <Route path="/register" component={RegisterPage} />
         <Route path="/login" component={SignInPage} />
       </Router>
