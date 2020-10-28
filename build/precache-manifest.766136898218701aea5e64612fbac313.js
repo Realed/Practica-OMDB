b6946c9543582f627a1bd753e8a371828ed620e6
Daniel Gonzalez <danielgonzalezt13@gmail.com>
2020-10-28 11:56:04 +0100

    Added Build folder files

---
+++ build/precache-manifest.766136898218701aea5e64612fbac313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8203dc042d9ce3cb9dfe590006b8f0ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4cb607d79ace9f8773f",
+    "url": "/static/js/2.68232691.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.68232691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f827d21f2137ae66e59",
+    "url": "/static/js/main.8c27b8dc.chunk.js"
+  },
+  {
+    "revision": "8e79d01cecc70a5969fe",
+    "url": "/static/js/runtime-main.f9237a90.js"
+  }
+]);
